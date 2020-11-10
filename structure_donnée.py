b1876ce5bb07cad006c749cd2fb6c2e46963a1a8
Simon3421 <71848350+Simon3421@users.noreply.github.com>
2020-11-10 16:53:58 +0100

    Update structure_donnée.py

--- structure_donnée.py
+++ structure_donnée.py
@@ -19,7 +19,8 @@ def dictionnary(nb_submarine):
   -----
   The function will generate a dictionnary with the submarine with his position (x and y) and his life
   """
-  
+  nb_sub = nb_submarine
+  #nb_sub is used to verify is there rest submarine or not
   for n in range(1, nb_submarine + 1):
     
     submarine[n] = {"life" : submarine_life, "x" : random.randint(0, 4), "y" : random.randint (0, 4)}
